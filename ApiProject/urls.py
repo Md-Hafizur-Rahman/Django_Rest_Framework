d9263249fe7c3e48b60540d89fa2ad0f157986b9
Md Hafizur Rahman <44112062+Md-Hafizur-Rahman@users.noreply.github.com>
2022-09-05 18:20:25 +0600

    login-api

--- ApiProject/urls.py
+++ ApiProject/urls.py
@@ -3,11 +3,13 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 from ApiApp.views import homeView
+from rest_framework.authtoken.views import obtain_auth_token
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", homeView),
     path('api/firstapp/', include('ApiApp.urls')),
-    path('api-auth/', include('rest_framework.urls'))
+    path('api-auth/', include('rest_framework.urls')),
+    path('api/login-api',obtain_auth_token)
     
     ]
 urlpatterns+=static(settings.MEDIA_URL,Document_root=settings.MEDIA_ROOT)

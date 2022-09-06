bbad731c683a3ed9c7797bfd73126b8aeaa2fad1
Md Hafizur Rahman <44112062+Md-Hafizur-Rahman@users.noreply.github.com>
2022-09-06 10:59:53 +0600

    JWT-Customizing token claims

--- ApiProject/urls.py
+++ ApiProject/urls.py
@@ -4,13 +4,19 @@ from django.conf import settings
 from django.conf.urls.static import static
 from ApiApp.views import homeView
 from rest_framework.authtoken.views import obtain_auth_token
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
+from .views import MyTokenObtainPairView
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", homeView),
     path('api/firstapp/', include('ApiApp.urls')),
     path('api-auth/', include('rest_framework.urls')),
-    path('api/login-api',obtain_auth_token)
-    
+    path('api/login-api',obtain_auth_token),
+    path('api/token/', MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     ]
 urlpatterns+=static(settings.MEDIA_URL,Document_root=settings.MEDIA_ROOT)
 urlpatterns+=static(settings.STATIC_URL,Document_root=settings.STATIC_ROOT)
